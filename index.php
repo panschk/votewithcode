34446df3d539692cd9da3d298a1d4a62a37d0531
Paul-Gabriel Müller <panschk@googlemail.com>
2020-08-21 20:57:24 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Foodsharing Hannover - Wahlformular</title></head>
+
+<body>
+<?php 
+
+require_once 'formr/class.formr.php';
+$form = new Formr('bootstrap');
+echo $form->create_form('Name, Email, Comments|textarea');
+?>
+
+</body>
+</html>
